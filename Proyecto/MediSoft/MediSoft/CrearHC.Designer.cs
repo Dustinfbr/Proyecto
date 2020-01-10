1e26fcb85d5184baa50beabe4546ff4fc7336b0d
CarlosTonato <CarlosTonato_EPN@users.noreply.github.com>
2020-01-10 02:14:03 -0500

    Cambios HC

--- Proyecto/MediSoft/MediSoft/CrearHC.Designer.cs
+++ Proyecto/MediSoft/MediSoft/CrearHC.Designer.cs
@@ -35,26 +35,23 @@
             this.lblPaciente = new System.Windows.Forms.Label();
             this.lblCI = new System.Windows.Forms.Label();
             this.lblAlergias = new System.Windows.Forms.Label();
-            this.ChBxAlergias = new System.Windows.Forms.CheckedListBox();
             this.lblFecha = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.ChBxEnfermedades = new System.Windows.Forms.CheckedListBox();
             this.lblEnfermedades = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.btnSignosVitales = new System.Windows.Forms.Button();
-            this.btnReceta = new System.Windows.Forms.Button();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnRegresar = new System.Windows.Forms.Button();
+            this.lblFechaNac = new System.Windows.Forms.Label();
+            this.RtxtAlergias = new System.Windows.Forms.RichTextBox();
+            this.RtxtEnfermedades = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // lblAdmin
             // 
             this.lblAdmin.AutoSize = true;
             this.lblAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.lblAdmin.Location = new System.Drawing.Point(253, 86);
+            this.lblAdmin.Location = new System.Drawing.Point(169, 56);
+            this.lblAdmin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAdmin.Name = "lblAdmin";
-            this.lblAdmin.Size = new System.Drawing.Size(240, 29);
+            this.lblAdmin.Size = new System.Drawing.Size(156, 20);
             this.lblAdmin.TabIndex = 7;
             this.lblAdmin.Text = "Crear Historia Clínica";
             // 
@@ -64,27 +61,30 @@
             this.lblMedisoft.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.lblMedisoft.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
             this.lblMedisoft.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblMedisoft.Location = new System.Drawing.Point(283, 12);
+            this.lblMedisoft.Location = new System.Drawing.Point(189, 8);
+            this.lblMedisoft.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMedisoft.Name = "lblMedisoft";
-            this.lblMedisoft.Size = new System.Drawing.Size(181, 37);
+            this.lblMedisoft.Size = new System.Drawing.Size(124, 26);
             this.lblMedisoft.TabIndex = 6;
             this.lblMedisoft.Text = "MEDISOFT";
             // 
             // lblNombrePaciente
             // 
             this.lblNombrePaciente.AutoSize = true;
-            this.lblNombrePaciente.Location = new System.Drawing.Point(47, 142);
+            this.lblNombrePaciente.Location = new System.Drawing.Point(31, 92);
+            this.lblNombrePaciente.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNombrePaciente.Name = "lblNombrePaciente";
-            this.lblNombrePaciente.Size = new System.Drawing.Size(131, 20);
+            this.lblNombrePaciente.Size = new System.Drawing.Size(89, 13);
             this.lblNombrePaciente.TabIndex = 20;
             this.lblNombrePaciente.Text = "Nombre Paciente";
             // 
             // lblCedula
             // 
             this.lblCedula.AutoSize = true;
-            this.lblCedula.Location = new System.Drawing.Point(47, 173);
+            this.lblCedula.Location = new System.Drawing.Point(31, 112);
+            this.lblCedula.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCedula.Name = "lblCedula";
-            this.lblCedula.Size = new System.Drawing.Size(59, 20);
+            this.lblCedula.Size = new System.Drawing.Size(40, 13);
             this.lblCedula.TabIndex = 21;
             this.lblCedula.Text = "Cédula";
             this.lblCedula.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -92,107 +92,52 @@
             // lblPaciente
             // 
             this.lblPaciente.AutoSize = true;
-            this.lblPaciente.Location = new System.Drawing.Point(199, 142);
+            this.lblPaciente.Location = new System.Drawing.Point(133, 92);
+            this.lblPaciente.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPaciente.Name = "lblPaciente";
-            this.lblPaciente.Size = new System.Drawing.Size(152, 20);
+            this.lblPaciente.Size = new System.Drawing.Size(102, 13);
             this.lblPaciente.TabIndex = 22;
             this.lblPaciente.Text = "Nombres y Apellidos";
             // 
             // lblCI
             // 
             this.lblCI.AutoSize = true;
-            this.lblCI.Location = new System.Drawing.Point(199, 173);
+            this.lblCI.Location = new System.Drawing.Point(133, 112);
+            this.lblCI.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCI.Name = "lblCI";
-            this.lblCI.Size = new System.Drawing.Size(108, 20);
+            this.lblCI.Size = new System.Drawing.Size(70, 13);
             this.lblCI.TabIndex = 23;
             this.lblCI.Text = "XXXXXXXXX";
             // 
             // lblAlergias
             // 
             this.lblAlergias.AutoSize = true;
-            this.lblAlergias.Location = new System.Drawing.Point(47, 250);
+            this.lblAlergias.Location = new System.Drawing.Point(31, 162);
+            this.lblAlergias.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAlergias.Name = "lblAlergias";
-            this.lblAlergias.Size = new System.Drawing.Size(66, 20);
+            this.lblAlergias.Size = new System.Drawing.Size(44, 13);
             this.lblAlergias.TabIndex = 24;
             this.lblAlergias.Text = "Alergias";
             this.lblAlergias.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // ChBxAlergias
-            // 
-            this.ChBxAlergias.FormattingEnabled = true;
-            this.ChBxAlergias.Items.AddRange(new object[] {
-            "Alergia1",
-            "Alergia2",
-            "Alergia3",
-            "Alergia4",
-            "Alergia5",
-            "Alergia6",
-            "Alergia7",
-            "Alergia(",
-            "Alergia9",
-            "Alergia10",
-            ".",
-            ".",
-            ".",
-            ".",
-            ".",
-            ".",
-            ".",
-            ".",
-            "AlergiaN"});
-            this.ChBxAlergias.Location = new System.Drawing.Point(51, 273);
-            this.ChBxAlergias.Name = "ChBxAlergias";
-            this.ChBxAlergias.Size = new System.Drawing.Size(673, 119);
-            this.ChBxAlergias.TabIndex = 25;
-            // 
             // lblFecha
             // 
             this.lblFecha.AutoSize = true;
-            this.lblFecha.Location = new System.Drawing.Point(47, 208);
+            this.lblFecha.Location = new System.Drawing.Point(31, 135);
+            this.lblFecha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(133, 20);
+            this.lblFecha.Size = new System.Drawing.Size(90, 13);
             this.lblFecha.TabIndex = 26;
             this.lblFecha.Text = "FechaNacimiento";
             this.lblFecha.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(203, 208);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(290, 26);
-            this.dateTimePicker1.TabIndex = 27;
-            this.dateTimePicker1.Value = new System.DateTime(2019, 12, 11, 0, 0, 0, 0);
-            // 
-            // ChBxEnfermedades
-            // 
-            this.ChBxEnfermedades.FormattingEnabled = true;
-            this.ChBxEnfermedades.Items.AddRange(new object[] {
-            "Hipertension",
-            "Cancer",
-            "Diabetes",
-            ".",
-            ".",
-            ".",
-            ".",
-            ".",
-            "..",
-            ".",
-            ".",
-            "..",
-            ".",
-            ".",
-            "EnferN"});
-            this.ChBxEnfermedades.Location = new System.Drawing.Point(51, 433);
-            this.ChBxEnfermedades.Name = "ChBxEnfermedades";
-            this.ChBxEnfermedades.Size = new System.Drawing.Size(673, 119);
-            this.ChBxEnfermedades.TabIndex = 29;
-            // 
             // lblEnfermedades
             // 
             this.lblEnfermedades.AutoSize = true;
-            this.lblEnfermedades.Location = new System.Drawing.Point(47, 410);
+            this.lblEnfermedades.Location = new System.Drawing.Point(31, 266);
+            this.lblEnfermedades.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEnfermedades.Name = "lblEnfermedades";
-            this.lblEnfermedades.Size = new System.Drawing.Size(221, 20);
+            this.lblEnfermedades.Size = new System.Drawing.Size(145, 13);
             this.lblEnfermedades.TabIndex = 28;
             this.lblEnfermedades.Text = "Enfermedades Pre-Existentes";
             this.lblEnfermedades.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -235,38 +180,62 @@
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(510, 725);
+            this.btnGuardar.Location = new System.Drawing.Point(363, 392);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(106, 36);
+            this.btnGuardar.Size = new System.Drawing.Size(71, 23);
             this.btnGuardar.TabIndex = 34;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
             // 
             // btnRegresar
             // 
-            this.btnRegresar.Location = new System.Drawing.Point(622, 725);
+            this.btnRegresar.Location = new System.Drawing.Point(438, 392);
+            this.btnRegresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegresar.Name = "btnRegresar";
-            this.btnRegresar.Size = new System.Drawing.Size(106, 36);
+            this.btnRegresar.Size = new System.Drawing.Size(71, 23);
             this.btnRegresar.TabIndex = 35;
             this.btnRegresar.Text = "Regresar";
             this.btnRegresar.UseVisualStyleBackColor = true;
             // 
+            // lblFechaNac
+            // 
+            this.lblFechaNac.AutoSize = true;
+            this.lblFechaNac.Location = new System.Drawing.Point(133, 135);
+            this.lblFechaNac.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFechaNac.Name = "lblFechaNac";
+            this.lblFechaNac.Size = new System.Drawing.Size(70, 13);
+            this.lblFechaNac.TabIndex = 36;
+            this.lblFechaNac.Text = "XXXXXXXXX";
+            // 
+            // RtxtAlergias
+            // 
+            this.RtxtAlergias.Location = new System.Drawing.Point(35, 182);
+            this.RtxtAlergias.Name = "RtxtAlergias";
+            this.RtxtAlergias.Size = new System.Drawing.Size(460, 81);
+            this.RtxtAlergias.TabIndex = 37;
+            this.RtxtAlergias.Text = "";
+            // 
+            // RtxtEnfermedades
+            // 
+            this.RtxtEnfermedades.Location = new System.Drawing.Point(35, 282);
+            this.RtxtEnfermedades.Name = "RtxtEnfermedades";
+            this.RtxtEnfermedades.Size = new System.Drawing.Size(460, 86);
+            this.RtxtEnfermedades.TabIndex = 38;
+            this.RtxtEnfermedades.Text = "";
+            // 
             // CrearHC
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 773);
+            this.ClientSize = new System.Drawing.Size(520, 426);
+            this.Controls.Add(this.RtxtEnfermedades);
+            this.Controls.Add(this.RtxtAlergias);
+            this.Controls.Add(this.lblFechaNac);
             this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.btnGuardar);
-            this.Controls.Add(this.btnReceta);
-            this.Controls.Add(this.btnSignosVitales);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.ChBxEnfermedades);
             this.Controls.Add(this.lblEnfermedades);
-            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.lblFecha);
-            this.Controls.Add(this.ChBxAlergias);
             this.Controls.Add(this.lblAlergias);
             this.Controls.Add(this.lblCI);
             this.Controls.Add(this.lblPaciente);
@@ -274,6 +243,7 @@
             this.Controls.Add(this.lblNombrePaciente);
             this.Controls.Add(this.lblAdmin);
             this.Controls.Add(this.lblMedisoft);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CrearHC";
             this.Text = "CrearHC";
             this.ResumeLayout(false);
@@ -290,16 +260,12 @@
         private System.Windows.Forms.Label lblPaciente;
         private System.Windows.Forms.Label lblCI;
         private System.Windows.Forms.Label lblAlergias;
-        private System.Windows.Forms.CheckedListBox ChBxAlergias;
         private System.Windows.Forms.Label lblFecha;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.CheckedListBox ChBxEnfermedades;
         private System.Windows.Forms.Label lblEnfermedades;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button btnSignosVitales;
-        private System.Windows.Forms.Button btnReceta;
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Label lblFechaNac;
+        private System.Windows.Forms.RichTextBox RtxtAlergias;
+        private System.Windows.Forms.RichTextBox RtxtEnfermedades;
     }
 }
